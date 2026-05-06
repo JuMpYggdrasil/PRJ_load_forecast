210e6331c23814028df13cbc0f738462c386636b
JuMpYggdrasil <70381321+JuMpYggdrasil@users.noreply.github.com>
2026-05-06 21:38:07 +0700

    Update check_cuda.py

--- check_cuda.py
+++ check_cuda.py
@@ -1,5 +1,8 @@
 # pip install --upgrade pip
+# old
 # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+# new
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 
 import torch
 print(torch.__version__)
